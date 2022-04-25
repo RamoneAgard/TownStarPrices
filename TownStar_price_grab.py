313c56266fd7079defbae8cc39248957656fca04
RamoneAgard <rhagard@wustl.edu>
2022-04-25 15:06:31 -0500

    testing done, almost ready to run for final result

--- TownStar_price_grab.py
+++ TownStar_price_grab.py
@@ -2,6 +2,7 @@
 # Vox chainID = (Vox Collectibles #) + 584
 #import requests
 import time
+import csv
 #from tkinter import E
 #from tkinter import Grid
 from bs4 import BeautifulSoup
@@ -11,81 +12,172 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common import exceptions
 
+'''
+basic custom error class
+'''
+class CustomError(Exception):
+    pass
+
+'''
+Input: list of selected BS4 tags from rarity.tools and the webdriver to use
+Ouput: list of dictionaries with rarity.tools listing info
+    {name: nft name, priceEth: opeansea price, Score: rarity score, Link: view link}
+    (Throws exception if view page cannot load)
+'''
+def scrapeCells(cellList, driver):
+    scrapeList = []
+    for cell in cellList:
+        cellChildren = cell.contents
+        link = cellChildren[2].a['href']
+        viewLink = hrefBase + link
+
+        driver.get(viewLink)
+        loaded = WebDriverWait(driver, 10).until( EC.presence_of_element_located(
+            (By.CSS_SELECTOR,'div.font-extrabold.text-green-500')))
+        if(not loaded):
+            raise CustomError("Something went wrong in scrapeCells function ")
+            
+        webEle = driver.find_element_by_css_selector('div.font-extrabold.text-green-500')
+        score = webEle.get_attribute("innerText")
+        name = str(cellChildren[4].string).strip()
+        price = str(cellChildren[6].a.string)[:-4]
+        scrapeList.append({
+            "Name" : name,
+            "Price(ETH)":  price,
+            "Score": score,
+            "Link" : viewLink,
+        })
+        break
+    return scrapeList
+
+'''
+Input: Webdriver to use
+Output: the WebElement associated with the next page results button
+        (Throws an exception if button is not found)
+'''
+def getNextBtn(driver):
+    if(WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+        (By.XPATH, "//div[@class='select-none smallBtn'][contains(text(),'Next')]")))):
+        return driver.find_element(By.XPATH,"//div[@class='select-none smallBtn'][contains(text(),'Next')]")
+    else:
+        raise CustomError("Something else went wrong retrieving the next button")
+
+
+# global values needed
 hrefBase =  "https://rarity.tools"
 options = webdriver.FirefoxOptions()
 options.add_argument('--headless')
 
-'''
-Input: list of selected BS4 tags from rarity.tools
-Ouput: list of dictionaries with rarity.tools listing info
-    {name: nft name, link: nft view link, price: opeansea price}
-'''
-# def scrapeCells(driver, soup, cellSelector):
-#     cellList = soup.select(cellSelector)
-#     infoList = []
-#     for cell in cellList:
-#         cellChildren = cell.contents
-#         link = cellChildren[2].a['href']
-#         driver.find_element((By.CSS_SELECTOR, "a[href='"+link+"']"))
-#         driver.click()
-#         name = str(cellChildren[4].string).strip()
-#         price = str(cellChildren[6].a.string)
-#         infoList.append({
-#             "name" : name,
-#             "link" : link,
-#             "price":  price
-#         })
-#     return infoList
-
-def scrapeCells(cellList):
-    infoList = []
-    for cell in cellList:
-        cellChildren = cell.contents
-        link = cellChildren[2].a['href']
-        name = str(cellChildren[4].string).strip()
-        price = str(cellChildren[6].a.string)
-        infoList.append({
-            "name" : name,
-            "link" : link,
-            "price":  price
-        })
-    return infoList
-
-# def nextReultsPage(driver, ):
-
+# create WebDriver and get desired rarity.tools page plus the filters
 driver = webdriver.Firefox()
 driver.get("https://rarity.tools/collectvox?filters=%24buyNow%24On%3Atrue%3B%26auction%24On%3Atrue")
 #driver.get("https://opensea.io/collection/town-star?search[sortAscending]=true&search[sortBy]=PRICE&search[stringTraits][0][name]=game&search[stringTraits][0][values][0]=Town%20Star&search[stringTraits][1][name]=category&search[stringTraits][1][values][0]=Building&search[stringTraits][1][values][1]=Crafting&search[stringTraits][1][values][2]=Farm%20Stands&search[stringTraits][1][values][3]=Storage&search[stringTraits][1][values][4]=Towers&search[stringTraits][1][values][5]=Solar%20Panels&search[stringTraits][1][values][6]=Trophy&search[stringTraits][1][values][7]=Fountains&search[stringTraits][1][values][8]=Death%20Row%20Records&search[stringTraits][1][values][9]=ElfBot&search[stringTraits][1][values][10]=Gala%20Music&search[stringTraits][1][values][11]=Trade%20Vehicles&search[stringTraits][1][values][12]=Exchange&search[stringTraits][1][values][13]=Snoop%20Dogg&search[stringTraits][1][values][14]=Misc&search[stringTraits][1][values][15]=Saltybot&search[stringTraits][1][values][16]=Galaverse%20Tickets&search[stringTraits][1][values][17]=Crafter")
-#time.sleep(8)
-soup = ''
-btnGroup = ''
+writeInfoToFile = True
+
+#list of total scraped info
+infoList = []
+clickCount = 1
+while(True):
+    # wait for results to load 
+    try:
+        loaded = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located(
+            (By.CSS_SELECTOR,'div.transform')))
+        if(not loaded):
+            print("Driver wait returned false")
+            break
+    except exceptions.TimeoutException:
+        print("Timeout loading page results")
+        writeInfoToFile = False
+        break
+
+    #save current page for navigation
+    currPage = driver.current_url
+
+    # soup the page source and extract result cells 
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    gridCells = soup.select(".transform")
+
+    # try to append more scrapped information to total list
+    try:
+        infoList.extend(scrapeCells(gridCells, driver))
+    except exceptions.TimeoutException:
+        print("Driver Timeout scraping page results")
+        writeInfoToFile = False
+        break
+    except CustomError as e:
+        print(e.__str__)
+        writeInfoToFile = False
+        break
+
+    #try to navigate to next results page
+    # if there is no next page, then the process ends
+    time.sleep(4)
+    driver.get(currPage)
+    try:
+        nextBtn = getNextBtn(driver)
+        if nextBtn != None:
+            for x in range(clickCount):
+                nextBtn.click()
+                time.sleep(1)
+            clickCount += 1
+        else:
+            print("Failed to get next button")
+            break
+    except exceptions.StaleElementReferenceException:
+        print("No next page")
+        print("End of Scraping")
+        break
+    except exceptions.NoSuchElementException:
+        print("Found but could not retrieve next button")
+        print("Something went wrong")
+        writeInfoToFile = False
+        break
+    except exceptions.TimeoutException:
+        print("driver timeout waiting for next button")
+        writeInfoToFile = False 
+        break
+    except CustomError as e:
+        print(e.__str__)
+        writeInfoToFile = False
+        break
+
+driver.quit()
+
+# Write the info to csv file if its still okay to
+if(writeInfoToFile):
+    with open("townstar_vox_nfts.csv", 'w', newline='', encoding='utf-8') as f:
+        headers = ['Name', 'Price(ETH)', 'Score', 'Link']
+        writer = csv.DictWriter(f, fieldnames=headers)
+        writer.writeheader()
+        writer.writerows(infoList)
+        
+
+
+#nextBtn = ''
 #waitCount = 0
-done = 0
-try: 
-    if(WebDriverWait(driver, 10).until(
-        EC.text_to_be_present_in_element((By.CSS_SELECTOR,'div.smallBtn.select-none'),'Next'))):
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-        btnGroup = driver.find_element_by_css_selector('div.smallBtn.select-none')
-    else:
-        print("Could not meet condition for wait")
-        done = 1
-except exceptions.TimeoutException:
-    print("Driver Wait timeout")
-    done = 1
-except exceptions.NoSuchElementException:
-    print("Could not find element")
-    done = 1
-except:
-    print("Something went wrong")
-    done = 1
+# done = 0
+# try: 
+#     if(WebDriverWait(driver, 10).until(
+#         EC.text_to_be_present_in_element((By.CSS_SELECTOR,'div.smallBtn.select-none'),'Next'))):
+#         nextBtn = driver.find_element_by_css_selector('div.smallBtn.select-none')
+#     else:
+#         print("Could not meet condition for wait")
+#         done = 1
+# except exceptions.TimeoutException:
+#     print("Driver Wait timeout")
+#     done = 1
+# except exceptions.NoSuchElementException:
+#     print("Could not find next button element")
+#     done = 1
+# except:
+#     print("Something went wrong")
+#     done = 1
 
-if(done == 1):
-    driver.close()
-    done = 2
-else:
-    print("did it")
-    print(btnGroup)
-
+# if(done == 1): 
+#     driver.close()
+#     done = 2
+# else:
+#     print("did it")
 # scrapeInfo = []
 # while(done == 0):
 #     try:
@@ -104,83 +196,3 @@ else:
 #         print("Something else went wrong")
 #         done = 1
 #     time.sleep(5)
-
-
-# btnGroup.click()
-# time.sleep(5)
-# btnGroup.click()
-# time.sleep(5)
-# btnGroup.click()
-# time.sleep(5)
-# btnGroup.click()
-# time.sleep(5)
-# btnGroup.click()
-# time.sleep(5)
-
-
-# while(len(btnGroup) < 1 and waitCount < 10):
-#     time.sleep(2)
-#     soup = BeautifulSoup(driver.page_source, 'html.parser')
-#     btnGroup = driver.find_elements_by_css_selector('.smallBtn.select-none')
-#     waitCount += 1
-#     print("loading..." + str(waitCount))
-
-# if(len(btnGroup) < 1):
-#     driver.close()
-
-# nextBtn = btnGroup[0]
-# time.sleep(5)
-# print(nextBtn)
-# nextBtn.click()
-# time.sleep(8)
-# nextBtn.click()
-# time.sleep(8)
-gridCells = soup.select(".transform")
-infoList = []
-# print(type(gridCells[0].contents[2].a['href']))
-currPage = driver.current_url
-count = 0
-for cell in gridCells:
-    cellChildren = cell.contents
-    link = cellChildren[2].a['href']
-    score = ""
-    viewLink = hrefBase + link
-    driver.get(viewLink)
-    try:
-        loaded = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR,'div.font-extrabold.text-green-500')))
-        if(loaded):
-            webEle = driver.find_element_by_css_selector('div.font-extrabold.text-green-500')
-            score = webEle.get_attribute("innerText")
-            print(score)
-            print(type(score))
-    except exceptions.TimeoutException:
-        print("Driver wait timeout -- view url")
-        done = 1
-        break
-    # webEle = driver.find_element(By.CSS_SELECTOR, linkSelector)
-    # print(webEle)
-    # webEle.click()
-    # print(linkAnchor)
-    name = str(cellChildren[4].string).strip()
-    price = str(cellChildren[6].a.string)
-    infoList.append({
-        "name" : name,
-        "price":  price,
-        "points": score,
-        "link" : viewLink,
-    })
-    count += 1
-    if (count >= 5):
-        break
-
-done = 1
-time.sleep(5)
-print(infoList)    
-#print(GridCells)
-#print(len(gridCells))
-#print(soup.prettify())
-
-if(done == 1):
-    driver.close()
-# m-1.lg:m-1.5.mb-2.border.overflow-hidden.transition-all.border-gray-300.dark:border-gray-800.rounded-md.shadow-md.hover:border-pink-600.hover:-translate-y-1.5.hover:shadow-lg.transform.bg-white.bgCard
